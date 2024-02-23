c6614e9709539feff93beb2ccab036718a5570d6
Ian Holloway <imsnazzy@gmail.com>
2024-02-22 20:58:43 -0500

    started filling in integrals

---
+++ gradG.py
@@ -0,0 +1,26 @@
+
+import sympy as sp
+
+
+#R = np.linalg.norm( x - y )
+#G = np.exp( 1j * k * R ) / ( 4 * np.pi * R )
+
+x1,x2,x3, y1,y2,y3 = sp.symbols("x1 x2 x3 y1 y2 y3", real=True)
+
+R = sp.sqrt( (x1-y1)**2 + (x2-y2)**2 + (x3-y3)**2 )
+
+k = sp.symbols("k",real=True)
+
+G = sp.exp( sp.I*k*R ) / (4*sp.pi*R)
+
+gradG = [ sp.diff(G,x) for x in [x1,x2,x3] ]
+
+gradgradG = [ [ sp.diff(dG,y) for y in [y1,y2,y3] ] for dG in gradG ]
+
+r = sp.symbols("R",real=True)
+
+simple_gradgradG = [ [ sp.simplify( exp.subs( R, r ) ) for exp in rowG ] for rowG in gradgradG ]
+
+# diagonal term
+""" ( (xi - yi)**2 * (R**2*k**2 + 3*I*R*k - 3) + R**2 + -I*R**3*k ) * exp(I*R*k)/(4*pi*R**5) """
+# note both real and imag parts of exp(I*R) are even functions because R>0
--- sparseMOM_Deltas.py
+++ sparseMOM_Deltas.py
@@ -38,14 +38,15 @@ facets = np.row_stack(facets)
 f.close()
 
 # Get Normals
-v1 = vertices[facets[:,1],:] - vertices[facets[:,0],:]
-v2 = vertices[facets[:,2],:] - vertices[facets[:,0],:]
-facetNormals = np.cross(v1, v2)
-facetAreas = np.linalg.norm(facetNormals,axis=1)
-for faceInd in range(nFacets):
-	facetNormals[faceInd,:] /= facetAreas[faceInd]
-	v1[faceInd,:] = np.linalg.norm(v1[faceInd])
-	v2[faceInd,:] = np.linalg.norm(v2[faceInd])
+bases = np.zeros((2*nFacets,3))
+bases[:nFacets] = vertices[facets[:,1],:] - vertices[facets[:,0],:]
+bases[nFacets:] = vertices[facets[:,2],:] - vertices[facets[:,0],:]
+#facetNormals = np.cross(v1, v2)
+#facetAreas = np.linalg.norm(facetNormals,axis=1)
+#for faceInd in range(nFacets):
+#	facetNormals[faceInd,:] /= facetAreas[faceInd]
+#	v1[faceInd,:] = np.linalg.norm(v1[faceInd])
+#	v2[faceInd,:] = np.linalg.norm(v2[faceInd])
 
 # Going to use constant basis functions within facets - v1 and v2 can be vector bases
 # use divergence theorem to write in terms of perimeter integral - this should entirely cancel out
@@ -74,29 +75,45 @@ scenarios = [PlaneWave( np.array([1,0,0]), np.array([0,0,1]), [ np.array([np.cos
 scenarios.extend([ PlaneWave( np.array([np.cos(th),np.sin(th),0]), np.array([0,0,1]) ) for th in np.linspace(0,np.pi,5) ])
 
 
-# Build Matrix and RHS(s)
+# Fill Matrix and RHS(s)
 def G( x, y ):
 	R = np.linalg.norm( x - y )
-	return np.exp( 1j * k * R ) / ( 4 * np.pi * R )
+	return np.exp( 1j * k * R ) / ( 4 * np.pi * R + 1e-15 )
+
+def gradgradG( x,y ):
+	return 1 # TODO
+
+def DoubleIntegrate( x,wx, y,wy, f ):
+	return wx.dot( f( x,y ).dot(wy) )
+
 
 A = np.zeros((2*nFacets,2*nFacets),dtype=np.complex128)
 b = np.zeros((2*nFacets,len(scenarios)),dtype=np.complex128)
-for i in range(nFacets):
-	testCentroid = np.mean( vertices[facets[i,:],:], axis=0 )
-	for j in range(nFacets):
-		# integral of G * V1 or G * V2
-		I = ...
-		A[2*i:2*i+2,j] = np.cross( facetNormals[i], I )
+for i in range(2*nFacets):
+	for j in range(2*nFacets):
+		facet_i = i % nFacets
+		facet_j = j % nFacets
+		# integral of  jwu G(x_i,x_j) vi^Tv_j + vi^T dxi dxj G(x_i,x_j) v_j / jwe
+		I1 = 1j * w * mu * bases[i].dot(bases[j]) * DoubleIntegrate( pts[facet_i],wts[facet_i], pts[facet_j],wts[facet_j], G )
+		if facet_i == facet_j : # Singularity!
+			# integral of -vi^T gradG vj^T nHat
+			# TODO
+			I2 = 
+		else:
+			I2 = DoubleIntegrate( pts[facet_i],wts[facet_i], pts[facet_j],wts[facet_j], lambda x,y: bases[i].dot(gradgradG(x,y).dot(bases[j])) )
+		I2 /= 1j * w * ep
+		A[i,j] = I1 + I2
 		
 	for j in range(len(scenarios)):
-		b[2*i:2*i+2,j] = -np.cross( facetNormals[i], scenarios[j].excitation( testCentroid ) )
+		# integral of vi^T scenarios[j].excitation( xi )
+		b[i,j] = wts[facet_i].dot( scenarios[j].excitation(pts[facet_i]).dot(bases[facet_i]) )
 
 # Factor and solve
 sols = np.linalg.solve( A,b )
 
+
 # post process
 mags = [[np.linalg.norm( np.exp( 1j * k * srcPts.dot( obs ) ) @ sols[:,3*j:3*(j+1)] ) for obs in scene.observations] for scene in scenarios]
-
 
 # Plot results
 #ax = plt.figure().add_subplot(projection='3d')
