e9dc89761043bdb0e1a7314b83d1e3c16a1c6b51
Ian Holloway <imsnazzy@gmail.com>
2024-02-24 01:04:09 -0500

    brought PlaneWave into PO

--- PO_in_100_lines.py
+++ PO_in_100_lines.py
@@ -17,3 +17,25 @@ facetNormals = np.cross(v1, v2)
 facetAreas = np.linalg.norm(facetNormals,axis=1)
 for faceInd in range(nFacets):
 	facetNormals[faceInd,:] /= facetAreas[faceInd]
+
+
+# Set up Scenario(s)
+c = 3e8
+w = 3e9
+k = w / c
+
+class PlaneWave:
+	def __init__(self, propDir, polVec, obs=None):
+		self.prop = propDir.copy()
+		self.polV = prolVec.copy()
+		
+		if obs is None:
+			self.observations = [-self.prop]
+		else:
+			self.observations = obs.copy()
+	
+	def excitation(self, xyz ):
+		return self.polV * np.exp( 1j * k * self.prop.dot( xyz ) )
+
+scenarios = [PlaneWave( np.array([1,0,0]), np.array([0,0,1]), [ np.array([np.cos(th),np.sin(th),0]) for th in np.linspace(0,np.pi,40) ] )]
+scenarios.extend([ PlaneWave( np.array([np.cos(th),np.sin(th),0]), np.array([0,0,1]) ) for th in np.linspace(0,np.pi,5) ])
