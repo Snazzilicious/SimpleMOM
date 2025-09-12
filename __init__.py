e8d544de4b58f4fae99c66e588dffa1890104bae
Ian Holloway <imsnazzy@gmail.com>
2025-09-11 20:44:30 -0400

    fixed init.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
-import Inputs
-import MeshUtils as Mesh
-import Geometry
-import EM_Utils as EM
-import ResultsUtils as Results
-import Logging
-import POSBR
-import IPO
+__all__ = ['EM_Utils.py', 'Geometry.py', 'Inputs.py', 'IPO.py',
+           'Logging.py', 'MeshUtils.py','POSBR.py', 'ResultsUtils.py']
+
+from . import Inputs
+from . import MeshUtils as Mesh
+from . import Geometry
+from . import EM_Utils as EM
+from . import ResultsUtils as Results
+from . import Logging
+from . import POSBR
+from . import IPO
