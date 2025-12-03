3266d5bb31736da326a5b83accf3c9c9ead9975e
iholloway <imsnazzy@gmail.com>
2025-12-03 15:28:13 -0500

    promoting working hmath stuff to main directory

--- Tests/test_hmath.py
+++ Tests/test_hmath.py
@@ -11,6 +11,76 @@ import hMatrixTRSM
 import hMatrixGETRF
 
 
+"""Tests of block boundary functions
+"""
+
+def test_num_blocks():
+	
+	assert hMatrix.num_blocks( 10, 23 ) == 2
+	
+	assert hMatrix.num_blocks( 10, 5 ) == 1
+	
+	assert hMatrix.num_blocks( 256, 2e6 ) == 7812
+
+
+def test_block_size():
+	
+	min_block_size = 200
+	length = 34567
+	all_sizes = [hMatrix.block_size( min_block_size, length, i ) for i in range(hMatrix.num_blocks( min_block_size, length ))]
+	
+	assert length == sum(all_sizes)
+	
+	min_block_size = 200
+	length = 100
+	all_sizes = [hMatrix.block_size( min_block_size, length, i ) for i in range(hMatrix.num_blocks( min_block_size, length ))]
+	
+	assert length == sum(all_sizes)
+
+
+def test_block_begins():
+	
+	min_block_size = 200
+	length = 368897
+	all_sizes = np.array([hMatrix.block_size( min_block_size, length, i ) for i in range(hMatrix.num_blocks( min_block_size, length ))])
+	
+	bounds = [ hMatrix.block_begin( min_block_size, length, i ) for i in range(hMatrix.num_blocks( min_block_size, length )+1) ]
+	
+	assert bounds[0] == 0
+	assert bounds[-1] == length
+	assert np.all( all_sizes == np.diff(bounds) )
+
+
+def test_block_index():
+	
+	min_block_size = 20
+	length = 369
+	bounds = [ hMatrix.block_begin( min_block_size, length, i ) for i in range(hMatrix.num_blocks( min_block_size, length )+1) ]
+	
+	for i in range(len(bounds)-1):
+		begin = bounds[i]
+		end = bounds[i+1]
+		
+		inds = np.array([ hMatrix.block_index( min_block_size, length, i ) for i in range(begin,end,1) ])
+		
+		assert np.all( inds == i )
+		
+
+def test_on_block_boundary():
+	
+	min_block_size = 20
+	length = 369
+	bounds = [ hMatrix.block_begin( min_block_size, length, i ) for i in range(hMatrix.num_blocks( min_block_size, length )+1) ]
+	
+	for i in range(length+1):
+		assert hMatrix.on_block_boundary( min_block_size, length, i ) == (i in bounds)
+	
+
+
+
+
+"""Functions to construct hMatrices for testing
+"""
 def rand_complex( shp ):
 	return 2*np.random.random( shp )-1 + 1j*( 2*np.random.random( shp )-1 )
 
@@ -140,6 +210,12 @@ def make_random_hMatrix( M, N, block_size, dense_diag=True ):
 	return A,tru_A
 
 
+
+
+
+"""Test constructing an hMatrix
+"""
+
 def test_construction():
 
 	seed = np.random.randint(0,10000)
@@ -170,6 +246,8 @@ def test_construction():
 
 
 
+"""Test GEMM
+"""
 
 def test_hMatrixGEMM_1():
 
@@ -230,6 +308,10 @@ def test_hMatrixGEMM_2():
 	assert np.max( np.abs( tru_dense_C - dense_C ) ) < 1e-9
 
 
+
+
+"""Test TRSM
+"""
 
 from scipy.linalg import solve_triangular
 
@@ -309,6 +391,8 @@ def test_hMatrixTRSM_1():
 
 
 
+"""Test GETRF
+"""
 
 def test_hMatrixGETRF_1():
 	seed = np.random.randint(0,10000)
